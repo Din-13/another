c312f904a7ab89b96d714e9445b442ec7a6d4624
Andrey Sozykin <sozykin@gmail.com>
2022-08-28 17:31:07 +0500

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -38,7 +38,7 @@ def print_predictions(preds):
 
 model = load_model()
 
-st.title('Классификация изображений')
+st.title('Классификация изображений Docker CI')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
